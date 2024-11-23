5015ca1d7ef73299640fcd368a8ad38e872bf971
Saul Blanco Tejero <saul.blanco@sngular.com>
2024-11-23 14:33:31 +0100

    fix: echo return value

--- commands/echo.c
+++ commands/echo.c
@@ -6,7 +6,7 @@
 /*   By: saul.blanco <sblanco-@student.42madrid.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 22:26:29 by sblanco-          #+#    #+#             */
-/*   Updated: 2024/12/16 21:06:08 by saul.blanco      ###   ########.fr       */
+/*   Updated: 2024/12/16 21:06:31 by saul.blanco      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	ft_echo(t_cmd *cmd)
 	if (!arg_node->next)
 	{
 		printf("\n");
-		return ;
+		return (0);
 	}
 	new_line = ft_strncmp((char *) arg_node->next->content, "-n", 2);
 	if (new_line)
