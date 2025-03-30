ec9fa385df7b3e329efbe32cad2762c3f82e7bcc
miguemoliaga <mmartine@c2r5s1.42madrid.com>
2025-03-30 19:01:01 +0200

    accesible files comprobation

--- tokenizer/utils.c
+++ tokenizer/utils.c
@@ -3,14 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sblanco- <sblanco-@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: mmartine <mmartine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 17:00:43 by mmartine          #+#    #+#             */
-/*   Updated: 2025/03/30 12:02:32 by sblanco-         ###   ########.fr       */
+/*   Updated: 2025/03/30 19:00:35 by mmartine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+bool	accesible_file(char	*filename, int access_mode)
+{
+	int	fd;
+
+	g_exit_num = 1;
+	if (access(filename, F_OK) < 0)
+	{
+		ft_putstr_fd("minishell: ", 2);
+		ft_putstr_fd(filename, 2);
+		return (ft_putstr_fd(": No such file or directory\n", 2), false);
+	}
+	else if (access(filename, access_mode) < 0)
+	{
+		ft_putstr_fd("minishell: ", 2);
+		ft_putstr_fd(filename, 2);
+		return (ft_putstr_fd(": Permission denied\n", 2), false);
+	}
+	fd = open(filename, O_DIRECTORY);
+	if (open >= 0)
+	{
+		close(fd);
+		ft_putstr_fd("minishell: ", 2);
+		ft_putstr_fd(filename, 2);
+		return (ft_putstr_fd(": Is a directory\n", 2), false);
+	}
+	g_exit_num = 0;
+	return (true);
+}
 
 size_t	ft_index_of_symbol(char *str)
 {
