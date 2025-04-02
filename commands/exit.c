2a87a16134d723391eb46ba334c2f35df8303640
miguemoliaga <mmartine@c2r5s3.42madrid.com>
2025-04-02 18:25:52 +0200

    true exit fix

--- commands/exit.c
+++ commands/exit.c
@@ -6,7 +6,7 @@
 /*   By: mmartine <mmartine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 22:37:07 by sblanco-          #+#    #+#             */
-/*   Updated: 2025/01/17 01:46:59 by mmartine         ###   ########.fr       */
+/*   Updated: 2025/04/02 18:25:17 by mmartine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 
 //GESTIONAR MAX LONG LONG Y MIN LONG LONG
 
-int	valid_exit(t_node *args, int arg_count)
+static int	valid_exit(t_node *args, int arg_count)
 {
 	int	i;
 
 	i = 0;
+	printf("exit\n");
 	if (!args || !args->next)
 		return (0);
+	if (((char *)args->next->content)[i] == '-')
+		i++;
 	while (((char *)args->next->content)[i])
 	{
 		if (!ft_isdigit(((char *)args->next->content)[i]))
@@ -32,24 +35,41 @@ int	valid_exit(t_node *args, int arg_count)
 	return (0);
 }
 
+static int	get_error_var_val(bool *jump, t_cmd *cmd)
+{
+	*jump = false;
+	if (cmd->args->next && !ft_strncmp((char *)cmd->args->next->content, "--",
+			ft_strlen((char *)cmd->args->next->content)))
+	{
+		*jump = true;
+		return (valid_exit(cmd->args->next, cmd->arg_count - 1));
+	}
+	else
+		return (valid_exit(cmd->args, cmd->arg_count));
+}
+
 int	ft_exit(t_shell *shell, t_cmd *cmd)
 {
-	int	error;
+	int		error;
+	bool	jump;
 
-	error = valid_exit(cmd->args, cmd->arg_count);
-	printf("exit\n");
+	error = get_error_var_val(&jump, cmd);
 	if (error == 1)
 	{
-		printf("bash: exit: too many arguments\n");
+		printf("minishell: exit: too many arguments\n");
 		return (1);
 	}
 	shell->exit_code = -1;
 	if (error == 2)
 	{
-		printf("bash: exit: %s: numeric argument required\n", cmd->bin);
+		printf("minishell: exit: %s: numeric argument required\n", cmd->bin);
 		return (2);
 	}
 	else if (cmd->args && cmd->args->next)
+	{
+		if (jump && cmd->args->next->next)
+			return (ft_atoi((char *)cmd->args->next->next->content));
 		return (ft_atoi((char *)cmd->args->next->content));
+	}
 	return (0);
 }
